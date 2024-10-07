607f72db4931676eabee9623d2e9abb6d162e051
Owen-Pryga <58037675+NotEnoughAuth@users.noreply.github.com>
2024-10-06 23:22:51 -0500

    added Yaml parsing functionality

---
+++ parse.py
@@ -0,0 +1,9 @@
+import yaml
+
+def parseYaml(yamlFile):
+    with open(yamlFile, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
